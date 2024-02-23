e2c2c826b673951e11f2781c23d1f04d448e30a8
iamjaswanth <jaswanthsrm@gmail.com>
2024-02-23 17:44:55 +0530

    hello world

--- src/main/scala/helloworld.scala
+++ src/main/scala/helloworld.scala
@@ -2,8 +2,47 @@ object helloworld {
 
   def main(args: Array[String])
   {
-    // prints Hello World
-    println("Hello World_2!")
+    for (i<-1 to 100){
+      println(i)
+    }
+    var i = 1
+    while (i <= 50) {
+      println(i)
+      i += 1
+    }
+
+    val numRows = 5
+
+    // Loop through each row
+    for (i <- 1 to numRows) {
+      // Loop to print numbers in each row
+      for (j <- 1 to i) {
+        print(j)
+        // Print a comma if it's not the last number in the row
+        if (j < i) {
+          print(",")
+        }
+      }
+      // Print a new line after each row
+      println()
+    }
+    var a = 1
+    // Loop through each row
+    for (i <- 1 to 5) {
+      // Loop to print numbers in each row
+      for (j <- 1 to i) {
+        print(a)
+        a= a+1
+        if (j < i) {
+          print(",")
+        }
+
+
+      }
+      // Print a new line after each row
+      println()
+    }
+
   }
 
 }
